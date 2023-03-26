73996d5477740565d18c06be3d8af58f3bc2eb88
codeS <scode1290@gmail.com>
2023-03-26 16:09:04 +0200

    Added all the Pictures in the assets folder

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './navbar.css'
+
+import {Logo} from '../../assets/Logo Proper.png'
 
 function Navbar() {
   return (
-    <div>
-      Navbar
+    <div className='navbar'>
+      <div className="logo">
+        <img src={Logo} alt="DrinkPrimeSA" />
+      </div>
+      <div className="text-slider">
+        
+      </div>
     </div>
   )
 }
