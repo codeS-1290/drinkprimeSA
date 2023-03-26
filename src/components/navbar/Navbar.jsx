a4400f63791323472c3e87d50cfe037bfe00c5cc
codeS <scode1290@gmail.com>
2023-03-27 00:15:34 +0200

    Decided to Finish Navbar by making options static (No slider) Also I added Nihaal's Product cards to my code. They are incomplete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import './navbar.css'
+import React from "react";
+import "./navbar.css";
 
-import {Logo} from '../../assets/Logo Proper.png'
+import Logo from "../../assets/logo.svg";
+import SA from "../../assets/sa.svg";
 
 function Navbar() {
   return (
-    <div className='navbar'>
-      <div className="logo">
-        <img src={Logo} alt="DrinkPrimeSA" />
-      </div>
+    <div className="navbar-section">
       <div className="text-slider">
-        
+        <div className="left-text">
+          <p>COMING SOON ON TAKEALOT</p>
+        </div>
+        <div className="logo">
+          <img src={Logo} alt="DrinkPrimeSA" />
+        </div>
+        <div className="right-text">
+          <p>
+            NATIONWIDE DELIVERY AVAILABLE
+            <img className="sa" src={SA} alt="South Africa" />
+          </p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
+
--- src/containers/cards/Cards.jsx
+++ src/containers/cards/Cards.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import  './cards.css';
 
-function Cards() {
-  return (
-    <div>
-      Cards
-    </div>
-  )
+function Cards(props) {
+  return (<div className='card'>
+    <h1>{props.title}</h1>
+    <div style={{display: 'flex', alignItems: 'center'} }></div>
+    <img src={props.imgName}></img>
+    <p>{props.description}</p>
+
+  </div>);
 }
 
 export default Cards
