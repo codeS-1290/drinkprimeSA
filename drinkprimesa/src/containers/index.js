dc8b3ff886c2e864534d6d4d0ef081e9ad883f2d
codeS <scode1290@gmail.com>
2023-03-27 22:58:03 +0200

    Merged Nihaal's and Abdullah's Local repositories to create a big full one

--- drinkprimesa/src/containers/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Blobs} from "./blobs/Blobs"
-export {default as Cards} from "./cards/Cards"
-
-
-/*we are exporting all these files as keywords to use in the components folder so that we can render them 
-with ease*/
